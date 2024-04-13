1de00f764c49f3ba197bb7108b6989bd37ddab07
pranshupandey19 <pranshumac19@gmail.com>
2024-04-13 14:09:38 +0530

    bug fix

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -32,7 +32,7 @@ paymentRouter.post(
 
 paymentRouter.post("/paymentverification/:id/:amount", async (req, res) => {
   let { id, amount } = req.params;
-  res.redirect(`https://community-connect-wbs6.vercel.app/thanks/${id}/${amount}`);
+  res.redirect(`https://community-connect-chads.vercel.app/thanks/${id}/${amount}`);
 });
 
 paymentRouter.get("/getkey", (req, res) => {
