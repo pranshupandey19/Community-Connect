93532cd354cc33f20f4f88930d745f2ef3296b61
aayushwrld <aayush.arora@kalvium.community>
2024-04-13 11:48:17 +0530

    Adding AI

--- frontend/src/components/Alerts.jsx
+++ frontend/src/components/Alerts.jsx
@@ -1,4 +1,13 @@
+import React, { useEffect, useState } from "react";
 import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Table,
   TableContainer,
   Tbody,
@@ -6,27 +15,62 @@ import {
   Th,
   Thead,
   Tr,
+  useDisclosure,
 } from "@chakra-ui/react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default function Alerts() {
-  document.title = "Alerts!"
+  const genAI = new GoogleGenerativeAI("AIzaSyCpI_eqmPLFjJX9omtdcF_zdyQsCDlqLn8");
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [generatedTips, setGeneratedTips] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedData, setSelectedData] = useState({});
   const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://sachet.ndma.gov.in/cap_public_website/FetchAllAlertDetails")
       .then((res) => {
         console.log(res.data);
-        setData(res.data);
+        setData(res.data.map((item) => ({ ...item, isLoading: false })));
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  async function run(index) {
+    setIsLoading(true);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const currentItem = data[index];
+    const prompt = `There is ${currentItem["disaster_type"]} in ${currentItem["alert_source"]} So I want you to give me a list of precautions I should take to save myself from the situation. Give me very specific points so that I am clear with my precautions. Don't make it longer than 100 words. Start with something positive. (Use <br> after every point. Use <b> tag to highlight important stuff. Please wrap heading around <b> tag instead of **)`;
+    console.log(prompt);
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    setGeneratedTips(text);
+    setIsLoading(false);
+    setSelectedData(data[index]);
+    onOpen();
+  }
+
+  function AIButton({ index }) {
+    const currentItem = data[index];
+    return (
+      <Button
+        isLoading={currentItem.isLoading}
+        onClick={() => run(index)}
+      >
+        Ask AI
+      </Button>
+    );
+  }
+
   return (
     <div className="alerts-parent">
-      {data.length == 0 ? (
+      {data.length === 0 ? (
         ""
       ) : (
         <TableContainer>
@@ -35,6 +79,7 @@ export default function Alerts() {
               <Tr>
                 <Th color={"red"}>Source</Th>
                 <Th color={"red"}>Type</Th>
+                <Th color={"red"}>Ask AI</Th>
                 <Th color={"red"}>Severity</Th>
               </Tr>
             </Thead>
@@ -43,6 +88,10 @@ export default function Alerts() {
                 <Tr key={i}>
                   <Td>{e["alert_source"]}</Td>
                   <Td>{e["disaster_type"]}</Td>
+                  
+                  <Td>
+                    <AIButton index={i} />
+                  </Td>
                   <Td>
                     <div
                       style={{
@@ -59,6 +108,21 @@ export default function Alerts() {
           </Table>
         </TableContainer>
       )}
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Tips from AI :)</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div dangerouslySetInnerHTML={{ __html: generatedTips }}></div>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
